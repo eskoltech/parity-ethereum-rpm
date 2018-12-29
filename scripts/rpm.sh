995ec71d8e32f66909716fed22cf53f36cd6a043
Javi <nhitbh@gmail.com>
2018-12-29 22:52:49 +0100

    Change owner of all files in the rpmbuild directory

--- scripts/rpm.sh
+++ scripts/rpm.sh
@@ -4,5 +4,6 @@ export HOME=/root
 sources_dir=$HOME/rpmbuild/SOURCES
 spec_dir=$HOME/rpmbuild/SPECS
 
+chown -R root:root $HOME/rpmbuild
 rpmbuild -ba $spec_dir/parity.spec
 rm -rf $sources_dir/*.tar.gz
