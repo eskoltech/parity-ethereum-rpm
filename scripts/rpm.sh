165ca1501f9027f14f6b14e1c7de876857ba69f1
Javi <nhitbh@gmail.com>
2018-12-29 18:08:13 +0100

    Show /etc/group in travis

--- scripts/rpm.sh
+++ scripts/rpm.sh
@@ -5,6 +5,9 @@ sources_dir=$HOME/rpmbuild/SOURCES
 spec_dir=$HOME/rpmbuild/SPECS
 
 id
+
+cat /etc/group
+
 sudo chown -R $(id -u):$(id -g) $HOME/rpmbuild
 rpmbuild -ba $spec_dir/parity.spec
 rm -rf $sources_dir/*.tar.gz
