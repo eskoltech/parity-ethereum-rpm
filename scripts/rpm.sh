18fdd9e05711fd6baa0ec80685bdb80b940658a3
Javi <nhitbh@gmail.com>
2018-12-29 21:24:04 +0100

    Remove unused command from rpm.sh script

--- scripts/rpm.sh
+++ scripts/rpm.sh
@@ -4,6 +4,5 @@ export HOME=/home/rpmuser
 sources_dir=$HOME/rpmbuild/SOURCES
 spec_dir=$HOME/rpmbuild/SPECS
 
-sudo chown -R $(id -u):$(id -g) $HOME/rpmbuild
 rpmbuild -ba $spec_dir/parity.spec
 rm -rf $sources_dir/*.tar.gz
