b71235146dc114e93c42ef22355c51641ecf851c
Javi <nhitbh@gmail.com>
2018-12-29 18:01:06 +0100

    Restore user permissions over sources directory

--- scripts/rpm.sh
+++ scripts/rpm.sh
@@ -4,5 +4,7 @@ export HOME=/home/rpmuser
 sources_dir=$HOME/rpmbuild/SOURCES
 spec_dir=$HOME/rpmbuild/SPECS
 
+id
+sudo chown -R $(id -u):$(id -g) $HOME/rpmbuild
 rpmbuild -ba $spec_dir/parity.spec
 rm -rf $sources_dir/*.tar.gz
--- scripts/tar.sh
+++ scripts/tar.sh
@@ -3,6 +3,14 @@
 export HOME=/home/rpmuser
 sources_dir=$HOME/rpmbuild/SOURCES
 
+# Fix ownership of output files
+finish() {
+    # Fix ownership of output files
+    user_id=$(stat -c '%u:%g' $sources_dir)
+    chown -R ${user_id} $sources_dir
+}
+trap finish EXIT
+
 rm -rf $sources_dir/*.tar.gz
 tar czf $sources_dir/parity-$PARITY_VERSION.tar.gz -C $sources_dir parity-$PARITY_VERSION
 ls -la $sources_dir
